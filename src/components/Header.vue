51e1d87e7ae0c60e3df30d473dc79330d0c163f3
Manoj Naidu <manojnaidu619@gmail.com>
2019-07-17 17:19:47 +0530

    Added cards

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -9,10 +9,10 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav mr-auto">
         <li class="nav-item">
-          <router-link to="/stocks" class="btn">Stocks</router-link>
+          <router-link to="/portfolio" class="btn">Portfolio</router-link>
         </li>
         <li class="nav-item">
-          <router-link to="/portfolio" class="btn">Portfolio</router-link>
+          <router-link to="/stocks" class="btn">Stocks</router-link>
         </li>
       </ul>
     </div>
--- src/components/Stocks.vue
+++ src/components/Stocks.vue
@@ -1,16 +1,53 @@
 <template lang="html">
   <div class="container">
-    <h1>Stock</h1>
+    <div class="row justify-content-center">
+      <div class="col-sm-6 col-md-4" v-for="stock in stocks">
+        <div class="card text-black mb-3" style="max-width: 18rem;" >
+          <div class="card-body">
+            <h5 class="card-title">{{stock.name}}<small>(price:{{stock.price}})</small></h5>
+            <div class="input-group mb-3">
+              <input type="text" class="form-control" placeholder="Enter Price" aria-label="Recipient's username" aria-describedby="button-addon2">
+              <div class="input-group-append">
+                <button class="btn" type="button">Buy!</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-}
+  data(){
+    return{
+      stocks: [
+        {id:1, name:'BMW', price:110},
+        {id:2, name:'Apple', price:100},
+        {id:3, name:'Facebook', price:90},
+        {id:4, name:'Tesla', price:105},
+        {id:5, name:'NASA', price:120},
+        {id:6, name:'Amazon', price:85}
+      ]
+    }
+  }
+
+ }
 </script>
 
 <style lang="css" scoped>
 .container{
-  margin-top:10px;
+  margin-top:50px;
+}
+.card{
+  background-color: Lavender;
+}
+.form-control:focus{
+  outline: 0 !important;
+  box-shadow: none;
+}
+.btn{
+  background-color: Chartreuse;
 }
 </style>
